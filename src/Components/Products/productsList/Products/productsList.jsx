3510b7301dc8d52e9371989e04e634d267641ef9
MuhamedBilal <mohamedbelal109@gmail.com>
2023-07-19 10:11:56 +0300

    products component

---
+++ src/Components/Products/productsList/Products/productsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './ProductsProductsList.css'
+function ProductsProductsList() {
+  return (
+    <div>ProductsProductsList</div>
+  )
+}
+
+export default ProductsProductsList
