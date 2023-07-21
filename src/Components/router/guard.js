d4c96d7441394be4c513a599884d4648254c0cf1
MuhamedBilal <mohamedbelal109@gmail.com>
2023-07-21 14:16:27 +0300

    nthin

--- src/Components/router/guard.js
+++ src/Components/router/guard.js
@@ -4,8 +4,6 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
-  console.log('PrivateRoute token:', token); // add this line to log the token
-
   const auth = token ? true : false;
   return auth ? children : <Navigate to="/signin" />;
 };
