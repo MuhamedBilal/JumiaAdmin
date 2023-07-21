c70e116646ece758cf9bdbe17fdf2ec4b15bd638
MuhamedBilal <mohamedbelal109@gmail.com>
2023-07-21 14:15:56 +0300

    fix private routes

--- src/Components/router/guard.js
+++ src/Components/router/guard.js
@@ -1,10 +1,11 @@
 
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('UserToken');
+  const token = localStorage.getItem('token');
+  console.log('PrivateRoute token:', token); // add this line to log the token
+
   const auth = token ? true : false;
   return auth ? children : <Navigate to="/signin" />;
 };
