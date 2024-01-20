f1ec08776f5be8fb5011a9e0fff65db42b2efc47
MuhamedBilal <73747468+MuhamedBilal@users.noreply.github.com>
2024-01-20 15:40:46 +0200

    Update guard.js

--- src/Components/router/guard.js
+++ src/Components/router/guard.js
@@ -1,11 +1,11 @@
 
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  const auth = token ? true : false;
-  return auth ? children : <Navigate to="/signin" />;
-};
+// const PrivateRoute = ({ children }) => {
+//   const token = localStorage.getItem('token');
+//   const auth = token ? true : false;
+//   return auth ? children : <Navigate to="/signin" />;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
